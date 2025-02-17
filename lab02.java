564985ba6802faaa1ecb56c64022ac639c3ebc5a
kvrm7 <kelsieverma@gmail.com>
2025-02-16 19:31:47 -0800

    Update lab02.java

--- lab02.java
+++ lab02.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  * 
  */
 
-public class FloatingPointConverter {
+public class lab02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
